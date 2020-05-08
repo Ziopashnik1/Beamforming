8ffc8b86477c639e703220b4b7b6830421f26f89
Timofey <Timofey@DESKTOP-USIR6Q7>
2020-05-08 15:18:32 +0300

    Исправление логики работы шумового сигнала

--- BeamService/Functions/NormalRandomSignal.cs
+++ BeamService/Functions/NormalRandomSignal.cs
@@ -1,26 +1,63 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
 
 namespace BeamService.Functions
 {
     public class NormalRandomSignal : AmplitudeSignalFunction
     {
-        private const int SamplesCount = 1024;
+        private const int SamplesCount = 1000000;
 
-        private readonly double[] f_RandomSamples;
-        private readonly Random f_Random = new Random();
+        private static readonly double[] __RandomSamples;
 
-        private double f_Mu;
-
-        public double Mu
+        static NormalRandomSignal()
         {
-            get => f_Mu;
-            set => Set(ref f_Mu, value);
+            var rnd = new Random();
+            __RandomSamples = rnd.NextNormal(SamplesCount);
         }
+
+        private readonly int _Step;
+        private int _Position;
+
+        private double _Mu;
+
+        public double Mu { get => _Mu; set => Set(ref _Mu, value); }
+
+        public int M = 0;
 
         public NormalRandomSignal() : this(0.01) { }
 
-        public NormalRandomSignal(double Amplitude) : base(Amplitude) => f_RandomSamples = f_Random.NextNormal(SamplesCount);
+        public NormalRandomSignal(double Amplitude) : base(Amplitude)
+        {
+            var rnd = new Random();
+            _Position = _Step = rnd.Next(17, 74);
+        }
 
-        public override double Value(double t) => f_RandomSamples[f_Random.Next(SamplesCount)] * Amplitude + f_Mu;
+
+        //public override double Value(double t)
+        //{
+        //    var N = f_Random.Next(SamplesCount);
+        //    var M = 0;
+        //    if (N == 0)
+        //    {
+        //        M = 1;
+        //    }
+
+        //    N += M;
+        //    var A = __RandomSamples[N] * Amplitude;
+        //    return A;
+        //}
+
+
+        public override double Value(double t)
+        {
+            _Position += _Step;
+            _Position %= SamplesCount;
+            return __RandomSamples[_Position] * Amplitude + _Mu;
+        }
+
+        //for (var i = 0; i<SamplesCount - 1; i++)
+        //{
+        //    var N = i;
+        //}
     }
 }
